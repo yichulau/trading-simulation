92521961c9d35597d4980ca69fc5b32c7e65a41d
valiantwlone <valiantwlone@gmail.com>
2023-11-27 18:27:20 +0800

    added prompt engineering

--- app/trading/components/chatInterface.tsx
+++ app/trading/components/chatInterface.tsx
@@ -2,7 +2,7 @@
 
 import { Button } from '@/components/ui/button';
 import { Textarea } from '@/components/ui/textarea';
-import React, { useRef, useState } from 'react';
+import React, { useRef, useState, useEffect } from 'react';
 import { ArrowRightIcon } from "@radix-ui/react-icons"
 
 const getRandomText = () => {
@@ -21,9 +21,11 @@ function generateRandomWord() {
     return text;
   }
 
+
+
 function ChatInterface() {
     const [inputValue, setInputValue] = useState('');
-    const [streamText, setStreamText] = useState('');
+    const [streamText, setStreamText] = useState("");
     const streamTimeoutRef : any = useRef(null);
   
     const handleInputChange = (e : any) => {
@@ -33,12 +35,16 @@ function ChatInterface() {
     const handleButtonClick = () => {
       if (streamTimeoutRef.current) {
         clearInterval(streamTimeoutRef.current); // Clear any existing streams
+
       }
+
       
       // Start streaming random words
       streamTimeoutRef.current = setInterval(() => {
-        setStreamText((prev) => prev + " " + generateRandomWord());
-      }, 100); // Adjust the interval as needed
+        setStreamText((prev) => prev + " " + createForexHackerString());
+      }, 500); // Adjust the interval as needed
+
+
   
       // Stop the stream after 5 seconds
       setTimeout(() => {
@@ -47,6 +53,89 @@ function ChatInterface() {
   
       setInputValue(''); // Clear the input field
     };
+
+    function generateRandomHexString(minLength: number, maxLength: number): string {
+      const characters: string = '0123456789ABCDEF';
+      const length: number = Math.floor(Math.random() * (maxLength - minLength + 1)) + minLength;
+      let result: string = '';
+      const charactersLength: number = characters.length;
+      for (let i: number = 0; i < length; i++) {
+        result += characters.charAt(Math.floor(Math.random() * charactersLength));
+      }
+      return result;
+    }
+    
+    function getRandomLength(min: number, max: number): number {
+      return Math.floor(Math.random() * (max - min + 1)) + min;
+    }
+    
+    function createForexHackerString(): string {
+      const prefixes: string[] = [
+        'FXCrunch_', 'TradeBot_', 'ForexSession_', 'MarketAnalyser_', 
+        'CurrencyPulse_', 'TradeMaster_', 'ForexInsight_', 'PipNavigator_',
+        'ChartWise_', 'MarketProbe_'
+      ];
+      
+      const forexTerms: string[] = [
+        'EURUSD', 'GBPUSD', 'USDJPY', 'AUDNZD', 'USDCAD', 'XAUUSD', 'Oil', 
+        'Yield', 'Pip', 'Lot', 'Fibonacci', 'Margin', 'Bearish', 'Bullish', 
+        'Resistance', 'Support', 'Leverage', 'Indicator', 'MACD', 'Rollover'
+      ];
+      
+      const commands: string[] = [
+        'analyze', 
+        'report', 
+        'compare', 
+        'correlation', 
+        'fetch', 
+        'backtest', 
+        'extract', 
+        'plot', 
+        'evaluate', 
+        'optimize', 
+        'identify'
+      ];
+      
+      const targets: string[] = [
+        '/analysis', '/data', '/archive', 
+        '/comparison', '/archives',
+        '/market', '/extraction', '/comparison', 
+        '/charts', '/performance'
+      ];
+      
+      const actions: string[] = [
+        'compiling', 'building', 'running', 'installing', 'configuring', 'cleaning',
+        'deploying', 'committing', 'executing', 'testing', 'removing', 'updating'
+      ];
+      
+      
+      const outcomes: string[] = [
+        'successful execution', 'improved performance', 'optimized workflow', 
+        'enhanced security', 'streamlined process'
+      ];
+    
+      // const prefix: string = prefixes[Math.floor(Math.random() * prefixes.length)];
+      // const forexTerm: string = forexTerms[Math.floor(Math.random() * forexTerms.length)];
+      // const command: string = commands[Math.floor(Math.random() * commands.length)];
+      // const target: string = targets[Math.floor(Math.random() * targets.length)];
+      // const action: string = actions[Math.floor(Math.random() * actions.length)];
+      // const outcome: string = outcomes[Math.floor(Math.random() * outcomes.length)];
+    
+      // Generate parts with more varied lengths
+      const hexString1: string = generateRandomHexString(getRandomLength(8, 16), getRandomLength(20, 32));
+      
+      // Assemble the parts into one string with variable lengths
+      const shuffledElements: string[] = [
+        ...prefixes, ...forexTerms, ...commands, ...targets, ...actions, ...outcomes, hexString1
+      ].sort(() => Math.random() - 0.5);
+    
+      const randomElements: string[] = shuffledElements.slice(0, 7); // Select 6 random elements
+    
+      // Join the selected elements to form the string
+      return randomElements.join('_');
+    }
+
+    
     
 
   return (
